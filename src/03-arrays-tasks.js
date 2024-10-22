9109029e708b050549b1e7cd748a4de19f166808
Nastia <the_sun.89@mail.ru>
2024-10-22 22:31:21 +0300

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -428,8 +428,14 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  // throw new Error('Not implemented');
+  return arr.sort((a, b) => {
+    if (a.country < b.country) return -1;
+    if (a.country > b.country) return 1;
+    if (a.city < b.city) return -1;
+    return 1;
+  });
 }
 
 /**
